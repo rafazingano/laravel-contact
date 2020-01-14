3835b0693784db5d27b6f3c2708bc9f459123721
rafazingano <rafazingano@gmail.com>
2020-01-14 17:56:47 -0300

    Update all

---
+++ src/Resources/ContactResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MeridienClube\Meridien\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ContactResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'content' => $this->content
+        ];
+    }
+}
