6c80405e9d08b706a81c2f69db698259f163f4cb
rafazingano <rafazingano@gmail.com>
2020-02-06 17:31:16 -0300

    Update all

--- src/Providers/ContactServiceProvider.php
+++ src/Providers/ContactServiceProvider.php
@@ -15,7 +15,7 @@ class ContactServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/../Views', 'contact');
+        //$this->loadViewsFrom(__DIR__ . '/../Views', 'contact');
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web.php');
         $this->loadMigrationsFrom(__DIR__ . '/../Databases/Migrations');
         $this->loadTranslationsFrom(__DIR__ . '/../Translations', 'contact');
